8e144ead8e1364ac74ea5ded4cfc43e8675ef00a
anukriti <anunwn@gmail.com>
2023-06-10 12:38:50 +0530

    done with minor project

--- minor_project_backend/controllers/blogController.js
+++ minor_project_backend/controllers/blogController.js
@@ -1,6 +1,8 @@
 const BlogPost = require('../models/BlogPost');
 const User = require('../models/User');
 const Comment = require('../models/Comment');
+const mongoose = require('mongoose');
+
 
 // Create a new blog post
 exports.createBlogPost = async (req, res) => {
@@ -30,16 +32,57 @@ exports.createBlogPost = async (req, res) => {
   }
 };
 
-// Get all blog posts
+// Get all blog posts with pagination and filtering
+
+
 exports.getAllBlogPosts = async (req, res) => {
   try {
-    const blogPosts = await BlogPost.find().populate('author', 'username');
+    const { page = 1, limit = 2, author } = req.query;
+console.log(req.query);
+    // Check if the author parameter is provided and is a non-empty string
+    if (author && typeof author === 'string' && author.trim() !== '') {
+      // Check if the author exists in the User collection
+      const authorExists = await User.exists({ _id: author });
 
-    res.status(200).json({ blogPosts });
+      // If the author doesn't exist, return an error response
+      if (!authorExists) {
+        return res.status(400).json({ message: 'Invalid author ID' });
+      }
+    }
+
+    // Construct the filter object based on the specified criteria
+    const filter = {};
+    if (author) {
+      filter.author = author;
+    }
+
+    // Fetch the total count of blog posts matching the filter
+    const totalCount = await BlogPost.countDocuments(filter);
+
+    // Apply pagination and filtering to the database query
+    const blogPosts = await BlogPost.find(filter)
+      .populate('author', 'username')
+      .skip((page - 1) * limit)
+      .limit(limit);
+
+    res.status(200).json({
+      totalCount,
+      currentPage: page,
+      totalPages: Math.ceil(totalCount / limit),
+      blogPosts
+    });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: 'An error occurred while getting all blog posts' });
   }
 };
+
+
+
+
+
+
+
 
 // Get a specific blog post by ID
 exports.getBlogPostById = async (req, res) => {
